693fd8d09b92306c629562a9095e6bb4a9402b51
2ssunny <andrew@ssunny.me>
2025-11-25 23:15:04 +0000

    feat: acceleration calculator

---
+++ acceleration.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+G = 8.6498928e-13 #Unit: km^3 kg^-1 hr^-2
+
+def acc_calculator(r, m):
+  N = len(m) #number of celestial objects
+  acc = np.zeros_like(r)
+
+  for i in range(N):
+    for j in range(N):
+      if i!=j:
+        dr = r[j]-r[i]
+        dist = np.sqrt(np.sum(dr**2))
+        acc[i] += G * m[j] * dr / (dist**3)
+  return acc
