d1584fed159d9ec517fc86dbad463613ece43abe
2ssunny <andrew@ssunny.me>
2025-11-30 06:48:32 +0000

    feat: improve acceleration calculation

--- acceleration.py
+++ acceleration.py
@@ -1,18 +1,20 @@
 import numpy as np
 
-G = 8.6498928e-13 #Unit: km^3 kg^-1 hr^-2
+G = 8.6498928e-13  #Unit: km^3 kg^-1 hr^-2
 
 def acc_calculator(r, m):
   """
     r: current postion, m: mass
    """
-  N = len(m) #number of celestial objects
-  acc = np.zeros_like(r)
+  dx = r[None, :, :] - r[:, None, :] 
 
-  for i in range(N):
-    for j in range(N):
-      if i!=j:
-        dr = r[j]-r[i]
-        dist = np.sqrt(np.sum(dr**2))
-        acc[i] += G * m[j] * dr / (dist**3)
+  dist_sq = np.sum(dx**2, axis=-1)
+
+  dist_sq += np.eye(len(r)) 
+  
+  dist = np.sqrt(dist_sq)
+
+  acc_matrix = G * (m[None, :] / (dist**3))[:, :, None] * dx
+  acc = np.sum(acc_matrix, axis=1)
+
   return acc
